ef4d03919b7c19711d396b7c6c56d83863717b46
Sebastián Javier Baetz <s128baetz@gmail.com>
2023-12-03 13:22:19 -0500

    new commit

--- app.js
+++ app.js
@@ -5,13 +5,47 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
-app.use(cors());
+app.use(cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+));
+
+
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+
+app.use(
+    session(sessionOptions)
+);
+
 CourseRoutes(app);
 
 app.use(express.json());
 ModuleRoutes(app);
+UserRoutes(app);
+
 
 Lab5(app)
 Hello(app)
